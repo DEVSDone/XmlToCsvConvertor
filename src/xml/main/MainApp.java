ee8fa15873a70484e02d85e24149012eaf6ddc4d
Dhiraj <patil.dhiraj101@gmail.com>
2019-05-22 12:06:17 +0530

    Changes done for reading 500 xml and meta files and writing there attributes into the csv file.

--- src/xml/main/MainApp.java
+++ src/xml/main/MainApp.java
@@ -5,6 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -13,11 +14,14 @@ import java.util.Set;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.opencsv.CSVWriter;
 
 import xml.ReadObj.ItemObject;
 import xml.ReadObj.ItemsObject;
 import xml.ReadObj.MetaDataObject;
+import xml.ReadObj.MetaFileObject;
 import xml.ReadObj.PartListObject;
 import xml.ReadObj.Property;
 
@@ -26,11 +30,22 @@ public class MainApp
 
 	static Set<String[]> allItems = new LinkedHashSet<String[]>();
 	//	static String folderPath="D:/JCI project/XMLToCsvConversion/PartListFolder/20May2019/500Testing";
-	static Path path = FileSystems.getDefault().getPath(".").toAbsolutePath();
-	//static String path = "D:/JCI project/XMLToCsvConversion/Testing";
+	static Path path = FileSystems.getDefault().getPath(".").toAbsolutePath(); //Original value for path 
+	//static String path = "D:/JCI project/XMLToCsvConversion/Testing";  //Testing purpose variable:
 	static String folderPath = path.toString();
 
 	public static void main(String[] args) 
+	{
+		System.out.println("Path: "+folderPath);
+		  LocalDateTime start = LocalDateTime.now();  
+		System.out.println("## Start Time: "+start.toString());
+		XmlAndMetaToCsv();
+		LocalDateTime end = LocalDateTime.now();  
+		System.out.println("## End Time: "+end.toString());
+	}
+
+	//Function : This function read xml and meta  files and take some values from both files and write into csv file.
+	private static void XmlAndMetaToCsv()
 	{
 		try
 		{
@@ -49,7 +64,216 @@ public class MainApp
 				String currentFile =folderPath+"/"+fname;			
 
 				File xmlfile = new File(currentFile);
-				
+
+				JAXBContext jaxbContext = JAXBContext.newInstance(PartListObject.class);
+				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+				PartListObject prtListObj = (PartListObject) jaxbUnmarshaller.unmarshal(xmlfile);
+				List<MetaDataObject> metaObjList = prtListObj.getMetaDataObject();
+				List<ItemsObject> itemsObjList = prtListObj.getItemsObject();
+
+				String partListNumber =" " ;  //PartList number 
+
+				//For loop for PartList Metadata
+				for (MetaDataObject metaDataObject : metaObjList)
+				{
+					//System.out.println("Meta Info :"+metaDataObject.getId());
+					if(metaDataObject.getId().contains("PartList_"))
+					{
+						List<Property> propertyList = metaDataObject.getPropertyList();				
+						for (Property propertyObject : propertyList) 
+						{
+							//System.out.println("Propery Info: "+propertyObject.getToken());
+							if(propertyObject.getToken().equals("number") )
+							{							
+								partListNumber = propertyObject.getValue();
+							}
+						}
+					}
+				}
+				//For loop for Items  Metadata
+				for (ItemsObject itemsObject : itemsObjList) 
+				{
+					List<ItemObject> itemList = itemsObject.getItemList();
+					if( (!(itemList == null) ) )
+					{
+
+						String se_Eff = getMeta_Ser_Effectivity(fname); //Getting Service Effectivity from 
+						
+					//	System.out.println("Se_Eff :"+se_Eff);
+						for (ItemObject itemObject : itemList) 
+						{
+							List<MetaDataObject> itemMetadataList = itemObject.getMetaDataObject();
+							String itemNum="";
+							String itemQty="";					
+							String partNumber="";
+							for (MetaDataObject metaDataObject : itemMetadataList)
+							{
+								//System.out.println("Meta Info :"+metaDataObject.getId());								
+								if(metaDataObject.getId().contains("WTPart_"))
+								{
+									List<Property> propertyList = metaDataObject.getPropertyList();				
+									for (Property propertyObject : propertyList) 
+									{
+										//System.out.println("Propery Info: "+propertyObject.getToken());
+										if(propertyObject.getToken().equals("number") )
+										{
+											//System.out.println("Metadata Part Number :"+propertyObject.getValue());
+											partNumber = propertyObject.getValue();
+										}
+									}
+								}
+								if(metaDataObject.getId().contains("PartListItem_"))
+								{
+									List<Property> propertyList = metaDataObject.getPropertyList();				
+									for (Property propertyObject : propertyList) 
+									{
+										if(propertyObject.getToken().equals("itemNumber") )
+										{
+											//	System.out.println(" itemNumber :"+propertyObject.getValue());
+											itemNum = propertyObject.getValue();
+										}
+										else if(propertyObject.getToken().equals("itemQuantity") )
+										{
+											//System.out.println(" itemQuantity :"+propertyObject.getValue());
+											itemQty = propertyObject.getValue();
+										}
+									}
+								}																
+							}
+						//	System.out.println("Values:"+partNumber+":"+itemNum+":"+itemQty+":");
+							splitMeta_Ser_Eff(se_Eff,partListNumber,partNumber,itemNum,itemQty);
+
+						}
+					}
+				}			
+				if(fileProcessCount%500 == 0)
+				{
+					System.out.println("########### Total Lines to write in csv file : "+allItems.size()+" ########### ");
+					writeMetaToCsvFile(allItems,fileProcessCount);
+					allItems.clear();
+				}
+			}
+
+			System.out.println("########### Total Lines to write in csv file : "+allItems.size()+" ########### ");
+			writeMetaToCsvFile(allItems,fileProcessCount);
+			allItems.clear();
+
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	private static String getMeta_Ser_Effectivity(String fname) 
+	{
+		String meta_Ser_Eff="";
+		//get meta file for that same xml name		
+		String	metaFileName = fname+".meta";
+		String currentFile =folderPath+"/"+metaFileName;			
+		try
+		{
+			File metaXmlFile = new File(currentFile);
+			System.out.println(" currentFile Meta File :"+metaFileName);			
+			if(metaXmlFile.exists() )
+			{
+				JAXBContext jaxbContext = JAXBContext.newInstance(MetaFileObject.class);
+				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+				//get root element Object from meta xml file 
+				MetaFileObject metaFileObj = (MetaFileObject) jaxbUnmarshaller.unmarshal(metaXmlFile);
+				//get Metadata elements under root element
+				List<MetaDataObject> metaObjList = metaFileObj.getMetaDataObject();			
+				//if  meta id contains "PartList_"
+				for (MetaDataObject metaDataObject : metaObjList)
+				{
+					if(metaDataObject.getId().contains("PartList_"))
+					{
+						List<Property> propertyList = metaDataObject.getPropertyList();				
+						for (Property propertyObject : propertyList) 
+						{
+							if(propertyObject.getToken().equals("ServiceEffectivity") )
+							{
+								//	System.out.println(" itemNumber :"+propertyObject.getValue());
+								meta_Ser_Eff = propertyObject.getValue();
+							}
+						}
+					}
+				}
+			}
+
+		} catch (Exception e)
+		{
+		//	System.out.println("Meta file not available for :"+fname);
+		//	System.out.println("Exception in getting metafile : "+e.getMessage() );
+		//	e.printStackTrace();
+		}
+
+		return meta_Ser_Eff;
+	}
+
+	private static void splitMeta_Ser_Eff(String se_Eff, String partListNumber, String partNumber, String itemNum,String itemQty) 
+	{
+
+		String[] sSplit = se_Eff.split("PRODUCT");
+		for (String string : sSplit) 
+		{
+			if(string.contains("INSTANCE"))
+			{
+			//	System.out.println("main SPlit :"+string+":");
+				String[] splitByProd = string.split(" "); 
+
+				String productName = splitByProd[1].replaceAll("[\\',)]", "");
+				//	System.out.println("##:ProductName Finally :"+ productName );
+				String[] splitForInstacne = string.split("INSTANCE");
+
+				String instances = splitForInstacne[1];
+				String[] inst = instances.split(" ");
+				for (String instance : inst) 
+				{
+					//System.out.println("Second SPlit :"+instance+":");
+					String singleInstName = StringUtils.substringBetween(instance,"'", "'");
+					if((!(singleInstName == null))  )
+					{
+					//	System.out.println("productName:"+productName+" :INSTANCE :"+singleInstName);
+
+						String[] foreachPrdDateRanges =new String[6]; 
+						foreachPrdDateRanges[0]=partListNumber;
+						foreachPrdDateRanges[1]= partNumber;
+						foreachPrdDateRanges[2]=itemNum ;					
+						foreachPrdDateRanges[3]=itemQty ;
+						foreachPrdDateRanges[4]=productName ;
+						foreachPrdDateRanges[5]=singleInstName;
+
+						allItems.add(foreachPrdDateRanges);
+
+					}
+				}
+			}
+		}
+	}
+
+	//Function : This function read only xml files and take some values and write into csv file.
+	public static void XmlToCsv()
+	{
+
+		try
+		{
+			int fileProcessCount = 0 ;
+			ArrayList<String> fileNames = getAllFilesFromFolder(folderPath);
+			for(String fname:fileNames)
+			{
+				String fileExtension = fname.substring(fname.lastIndexOf("."));				
+				if(!fileExtension.equals(".xml") )
+				{
+					continue;
+				}
+
+				System.out.println("################## File in Process : "+fileProcessCount++ +" File Name : "+fname);
+
+				String currentFile =folderPath+"/"+fname;			
+
+				File xmlfile = new File(currentFile);
+
 				JAXBContext jaxbContext = JAXBContext.newInstance(PartListObject.class);
 				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 				PartListObject prtListObj = (PartListObject) jaxbUnmarshaller.unmarshal(xmlfile);
@@ -127,8 +351,8 @@ public class MainApp
 									}
 								}																
 							}
-						//	System.out.println("Values:"+partNumber+":"+itemNum+":"+itemQty+":"+se_Eff);
-							splitForEachContext(se_Eff,partListNumber,partNumber,itemNum,itemQty);
+							//	System.out.println("Values:"+partNumber+":"+itemNum+":"+itemQty+":"+se_Eff);
+							splitXml_Ser_Eff(se_Eff,partListNumber,partNumber,itemNum,itemQty);
 						}
 					}
 				}			
@@ -148,8 +372,8 @@ public class MainApp
 		{
 			e.printStackTrace();
 		}
-	}
 
+	}
 	private static void writeCSVFile(Set<String[]> allItems2,int CountAsNameOfFile) 
 	{
 		String fileName = String.valueOf(CountAsNameOfFile)+".csv";
@@ -176,8 +400,34 @@ public class MainApp
 		} 
 
 	}
+	private static void writeMetaToCsvFile(Set<String[]> allItems2,int CountAsNameOfFile) 
+	{
+		String fileName = String.valueOf(CountAsNameOfFile)+".csv";
+		System.out.println("#######   Writing into csv file is going on ######### "+fileName);
+		File file = new File(folderPath+"/"+fileName); 
 
-	private static void splitForEachContext(String se_Eff,String partListNum,String partNumber,String itemNum,String itemQty) 
+		List<String[]> list = new ArrayList<String[]>(allItems2);
+		try { 
+			// create FileWriter object with file as parameter 
+			FileWriter outputfile = new FileWriter(file); 
+
+			// create CSVWriter object filewriter object as parameter 
+			CSVWriter csvWriter = new CSVWriter(outputfile); 
+			csvWriter.writeNext(new String[] { "PartList_Number","Part_Number","itemNumber","itemQuantity","Model_Number","Serial_Number"});
+			csvWriter.writeAll(list); 
+
+			// closing writer connection 
+			csvWriter.close(); 
+			System.out.println("#######   Writing into csv file is FINISH ######### ");
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace(); 
+		} 
+
+	}
+
+	private static void splitXml_Ser_Eff(String se_Eff,String partListNum,String partNumber,String itemNum,String itemQty) 
 	{
 
 		String[] sSplit = se_Eff.split("PRODUCT");
--- src/xml/testClass/test.java
+++ src/xml/testClass/test.java
@@ -1,7 +1,7 @@
 package xml.testClass;
 
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class test
 
@@ -9,82 +9,33 @@ public class test
 	public static void main(String[] args) 
 	{
 
-		   String name = "asfsa.xml.meta";
-          String extension = name.substring(name.lastIndexOf("."));
-          System.out.println("File Ext. :"+extension);
-	/*	int fileProcessCount= 8232;
-		for(int i= 1;i<=fileProcessCount ;i++)
-		if(i%1000 == 0)
-		{
-			System.out.println(" i: "+i);
-		}else if(i == fileProcessCount)
-		{
-			
-			
-				System.out.println(" i: "+i);
-			
-		}*/
-		
-		
-/*		Set<String[]> allItems = new LinkedHashSet<String[]>();
+		String   se_Eff = "(OR (AND (PRODUCT 'YKHHGBJ1CYF') (INSTANCE 'SFPM851140' 'SFPM850990')) (AND (PRODUCT 'YCUL0120EC46XCB') (INSTANCE '2NTM002437' '2LTM001975' '2LTM001972' '2LTM001974' '2FVM003749' '2GTM001239' '2HTM001559' '2LTM001973')) (AND (PRODUCT 'YVAA0153BAV46AA') (INSTANCE '2FXM011981' '2FXM011980'))";
 
-		String s ="(OR (AND (PRODUCT 'BRCS0421BD_PH') (OR (DATE_RANGE '2008-05-26 00:00:00.0' '2008-09-05 00:00:00.0')"
-				+ " (DATE_RANGE '2004-09-28 00:00:00.0' '2005-11-09 00:00:00.0') (DATE_RANGE '2008-05-26 00:00:00.0' '2008-09-05 00:00:00.0') "
-				+ "(DATE_RANGE '1901-01-01 00:00:00.0' '2004-09-28 00:00:00.0') (DATE_RANGE '2004-09-28 00:00:00.0' '2008-05-26 00:00:00.0') "
-				+ "(DATE_RANGE '2004-09-28 00:00:00.0' '2005-11-09 00:00:00.0') (DATE_RANGE '2004-09-28 00:00:00.0' '2008-05-26 00:00:00.0')))"
-				+ " (AND (PRODUCT 'BRCS0182BE_PH') (DATE_RANGE '1901-01-01 00:00:00.0' '2004-09-28 00:00:00.0')) (AND (PRODUCT 'BRCS0301BD_PH')"
-				+ " (OR (DATE_RANGE '2004-09-28 00:00:00.0' '2005-11-09 00:00:00.0') (DATE_RANGE '2004-09-28 00:00:00.0' '2008-05-26 00:00:00.0') "
-				+ "(DATE_RANGE '2008-05-26 00:00:00.0' '2008-09-05 00:00:00.0') (DATE_RANGE '2004-09-28 00:00:00.0' '2005-11-09 00:00:00.0')"
-				+ " (DATE_RANGE '1901-01-01 00:00:00.0' '2004-09-28 00:00:00.0') (DATE_RANGE '2008-05-26 00:00:00.0' '2008-09-05 00:00:00.0')"
-				+ " (DATE_RANGE '2004-09-28 00:00:00.0' '2008-05-26 00:00:00.0')))";
-
-		String[] sSplit = s.split("PRODUCT");
-
+		String[] sSplit = se_Eff.split("PRODUCT");
 		for (String string : sSplit) 
 		{
-			if(string.contains("DATE_RANGE"))
+			if(string.contains("INSTANCE"))
 			{
-			//	System.out.println("main SPlit :"+string+":");
+				System.out.println("main SPlit :"+string+":");
 				String[] splitByProd = string.split(" "); 
 
-				//	System.out.println("ProductName :"+splitByProd[1]);
 				String productName = splitByProd[1].replaceAll("[\\',)]", "");
-			//	System.out.println("##:ProductName Finally :"+ productName );
+				//	System.out.println("##:ProductName Finally :"+ productName );
+				String[] splitForInstacne = string.split("INSTANCE");
 
-
-				String[] splitForDate = string.split("DATE_RANGE"); 
-
-				for (String string2 : splitForDate) 
+				String instances = splitForInstacne[1];
+				String[] inst = instances.split(" ");
+				for (String instance : inst) 
 				{
-					if(string2.contains("00:00:00"))
+					//System.out.println("Second SPlit :"+instance+":");
+					String singleInstName = StringUtils.substringBetween(instance,"'", "'");
+					if((!(singleInstName == null))  )
 					{
-
-					//	System.out.println(":Date SPlit  :"+string2);
-						String[] foreachPrdDateRanges =new String[3];  //For each se_eff 
-						
-						String[] dates = string2.split(" ");
-						System.out.println(dates.length);
-						String fromDate = dates[1];
-						String toDate = dates[3];
-						foreachPrdDateRanges[0]=productName ;
-						foreachPrdDateRanges[1]=fromDate.replaceAll("[\\',)]", "");
-						foreachPrdDateRanges[2]=toDate.replaceAll("[\\',)]", "");
-
-						allItems.add(foreachPrdDateRanges);
-
+						System.out.println("productName:"+productName+" :INSTANCE :"+singleInstName);
 					}
 				}
 			}
 		}
-		System.out.println("###################################");
-			Iterator<String[]> itr = allItems.iterator();
-		while(itr.hasNext())
-		{
-				String val[] =itr.next();
-				System.out.println(":: "+val[0]+": "+val[1]+" : "+val[2]);
-		}
-
-*/
 	}
 
 }
